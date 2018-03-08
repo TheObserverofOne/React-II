0f4ff2bbd49afe65281218567ce908ffb584286a
KJ Sourcing <tmkogwork@gmail.com>
2018-03-07 22:29:09 -0800

    update all the things

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import CommentSection from "../CommentSection/CommentSection.js";
+
+const PostContainer = props => {
+	const post = props.insta;
+	return (
+		<div>
+			<div>
+				<div>
+					<img src={post.thumbnailUrl} alt="Instagram" />{" "}
+					{post.username}
+				</div>
+				<div className="instaImg">
+					<img src={post.imageUrl} alt="Instagram" />
+				</div>
+				<div>{post.likes}</div>
+				<div>
+					<CommentSection words={post.comments} />
+				</div>
+				<div>{post.timestamp}</div>
+			</div>
+		</div>
+	);
+};
+
+export default PostContainer;
